45bf547dbd273d00eeabcf9bc44a25c184b9528f
Raj Patel <raj.patel514@gmail.com>
2024-11-10 00:50:04 -0500

    Added form validation. Last Name is optional. Commented out.

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+//Form validation
+const fnIput = document.getElementById("firstName");
+const lnIput = document.getElementById("lastName");
+const quesInput = document.getElementById("questions");
+const emailInput = document.getElementById("email");
+const saveButton = document.getElementById("save-btn");
+const formInput = document.getElementById('form')
+
+function validateEmail(email) {
+    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    return re.test(String(email));
+}
+
+if (saveButton) {
+    saveButton.addEventListener("click", function (event) {
+        event.preventDefault();
+        let firstName = fnIput.value;
+        let lastName = lnIput.value;
+        let question = quesInput.value;
+        let email = emailInput.value;
+
+        if (firstName && lastName && question && email) {
+            if (validateEmail(email)) {
+                alert("Thank you for your submission. We will get back to you shortly!");
+                window.location.href = "index.html";
+            } else {
+                alert("Please enter a valid email address.");
+            }
+        } else {
+            if (!firstName) {
+                alert("Please enter your first name.");
+            } else if (!lastName) {
+                alert("Please enter your last name.");
+            } else if (!question) {
+                alert("Please enter your feedback or questions.");
+            } else if (!email) {
+                alert("Please enter your email address.");
+            }
+        }
+    });
+}

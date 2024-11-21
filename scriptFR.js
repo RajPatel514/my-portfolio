3398879ae95392f3319aab0ab59f2fe8bd133ad1
Raj Patel <raj.patel514@gmail.com>
2024-11-20 22:55:05 -0500

    Added Dark theme Images for CSS

--- scriptFR.js
+++ scriptFR.js
@@ -81,3 +81,9 @@ var typed = new Typed('.element', {
     shuffle: true,
 
 });
+
+// Dark Mode
+const toggleThemeButton = document.getElementById('darkMode');
+toggleThemeButton.addEventListener('click', function () {
+    document.body.classList.toggle('dark-mode');
+});

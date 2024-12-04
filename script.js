35463b774b8e4c11b30e68f1785c631b308d594b
Raj Patel <raj.patel514@gmail.com>
2024-12-03 23:19:49 -0500

    Ran JS Minifier on both scripts

--- script.js
+++ script.js
@@ -1,98 +1,64 @@
-//Form validation
-const fnIput = document.getElementById("firstName");
-// const lnIput = document.getElementById("lastName"); //Not used! I decide to make the last name optional
-const quesInput = document.getElementById("questions");
-const emailInput = document.getElementById("email");
-const saveButton = document.getElementById("save-btn");
-const formInput = document.getElementById('form');
-const translatePage = document.getElementById('translate');
-
-function validateEmail(email) {
-    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return re.test(String(email));
+const fnIput = document.getElementById("firstName"),
+    quesInput = document.getElementById("questions"),
+    emailInput = document.getElementById("email"),
+    saveButton = document.getElementById("save-btn"),
+    formInput = document.getElementById("form"),
+    translatePage = document.getElementById("translate");
+function validateEmail(e) {
+    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(String(e));
 }
-
-if (saveButton) {
-    saveButton.addEventListener("click", function (event) {
-        event.preventDefault();
-        let firstName = fnIput.value;
-        // let lastName = lnIput.value; 
-        let question = quesInput.value;
-        let email = emailInput.value;
-
-        if (firstName /*&& lastName*/ && question && email) {
-            if (validateEmail(email)) {
-                alert("Thank you for your submission. We will get back to you shortly!");
-                window.location.href = "index.html";
-            } else {
-                alert("Please enter a valid email address.");
-            }
-        } else {
-            if (!firstName) {
-                alert("Please enter your first name.");
-                // } else {
-                //     if (!lastName) {
-                //         alert("Please enter your last name.");
-            } else {
-                if (!question) {
-                    alert("Please enter your feedback or questions.");
-                } else {
-                    if (!email) {
-                        alert("Please enter your email address.");
-                    }
-                }
-                // }
-            }
-        }
+saveButton &&
+    saveButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        let t = fnIput.value,
+            n = quesInput.value,
+            a = emailInput.value;
+        t && n && a
+            ? validateEmail(a)
+                ? (alert(
+                    "Thank you for your submission. We will get back to you shortly!"
+                ),
+                    (window.location.href = "index.html"))
+                : alert("Please enter a valid email address.")
+            : t
+                ? n
+                    ? a || alert("Please enter your email address.")
+                    : alert("Please enter your feedback or questions.")
+                : alert("Please enter your first name.");
+    }),
+    translatePage.addEventListener("click", function () {
+        window.location.href = "indexFR.html";
     });
-}
-
-//Translation feature to go to french page
-translatePage.addEventListener('click', function () {
-    window.location.href = 'indexFR.html';
-});
-
-//Code for collapsible projects
-//Code learned from W3
-var dropdowns = document.getElementsByClassName("dropdown");
-
-for (var i = 0; i < dropdowns.length; i++) {
+for (
+    var dropdowns = document.getElementsByClassName("dropdown"), i = 0;
+    i < dropdowns.length;
+    i++
+)
     dropdowns[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
+        var e = this.nextElementSibling;
+        "block" === e.style.display
+            ? (e.style.display = "none")
+            : (e.style.display = "block");
     });
-}
-
-//Typed JS Features
-var typed = new Typed('.element', {
-    strings: ["Hey everyone! My name is Raj Patel, and I am studying computer programming at Humber College. I've taken a big step in a new direction to learn something new! This discipline will eventually help me make a shift in my career. I am currently working on a few web development projects on the side, as well as being a self - employed contractor. I was a machinist for five years prior to that.I am looking forward to what and where this new journey will bring me, thank you for visiting my page!"],
+var typed = new Typed(".element", {
+    strings: [
+        "Hey everyone! My name is Raj Patel, and I am studying computer programming at Humber College. I've taken a big step in a new direction to learn something new! This discipline will eventually help me make a shift in my career. I am currently working on a few web development projects on the side, as well as being a self - employed contractor. I was a machinist for five years prior to that.I am looking forward to what and where this new journey will bring me, thank you for visiting my page!",
+    ],
     typeSpeed: 10,
     startDelay: 500,
-    showCursor: true,
-    shuffle: true,
-
+    showCursor: !0,
+    shuffle: !0,
 });
-
-// Dark Mode
-const toggleThemeButton = document.getElementById('darkMode');
-toggleThemeButton.addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-});
-
-
-// Display Date-Time
+const toggleThemeButton = document.getElementById("darkMode");
 function displayTime() {
     function updateTime() {
-        let currentDate = new Date();
-        let formatTime = currentDate.toUTCString();
-        document.getElementById('currentTime').innerText = formatTime;
+        let e = new Date().toUTCString();
+        document.getElementById("currentTime").innerText = e;
     }
-    updateTime();
-    setInterval(updateTime, 1000);
+    updateTime(), setInterval(updateTime, 1e3);
 }
-displayTime();
+toggleThemeButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+}),
+    displayTime();
--- scriptFR.js
+++ scriptFR.js
@@ -1,101 +1,64 @@
-//Form validation
-const fnIput = document.getElementById("firstName");
-// const lnIput = document.getElementById("lastName"); //Not used! I decide to make the last name optional
-const quesInput = document.getElementById("questions");
-const emailInput = document.getElementById("email");
-const saveButton = document.getElementById("save-btn");
-const formInput = document.getElementById('form');
-const translatePage = document.getElementById('translate');
-
-function validateEmail(email) {
-    const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-    return re.test(String(email));
+const fnIput = document.getElementById("firstName"),
+    quesInput = document.getElementById("questions"),
+    emailInput = document.getElementById("email"),
+    saveButton = document.getElementById("save-btn"),
+    formInput = document.getElementById("form"),
+    translatePage = document.getElementById("translate");
+function validateEmail(e) {
+    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(String(e));
 }
-
-if (saveButton) {
-    saveButton.addEventListener("click", function (event) {
-        event.preventDefault();
-        let firstName = fnIput.value;
-        // let lastName = lnIput.value; 
-        let question = quesInput.value;
-        let email = emailInput.value;
-
-        if (firstName /*&& lastName*/ && question && email) {
-            if (validateEmail(email)) {
-                alert("Merci pour votre soumission. Nous vous répondrons sous peu!");
-                window.location.href = "indexFR.html";
-            } else {
-                alert("S'il vous plaît, mettez une courriel valide.");
-            }
-        } else {
-            if (!firstName) {
-                alert("Veuillez saisir votre prénom.");
-                // } else {
-                //     if (!lastName) {
-                //         alert("Veuillez entrer votre nom de famille.");
-            } else {
-                if (!question) {
-                    alert("Veuillez saisir vos commentaires ou vos questions.");
-                } else {
-                    if (!email) {
-                        alert("Veuillez entrer votre courriel.");
-                    }
-                }
-                // }
-            }
-        }
+saveButton &&
+    saveButton.addEventListener("click", function (e) {
+        e.preventDefault();
+        let t = fnIput.value,
+            n = quesInput.value,
+            a = emailInput.value;
+        t && n && a
+            ? validateEmail(a)
+                ? (alert("Merci pour votre soumission. Nous vous répondrons sous peu!"),
+                    (window.location.href = "indexFR.html"))
+                : alert("S'il vous plaît, mettez une courriel valide.")
+            : t
+                ? n
+                    ? a || alert("Veuillez entrer votre courriel.")
+                    : alert("Veuillez saisir vos commentaires ou vos questions.")
+                : alert("Veuillez saisir votre prénom.");
+    }),
+    translatePage.addEventListener("click", function () {
+        "/index.html" === window.location.pathname
+            ? (window.location.href = "indexFR.html")
+            : (window.location.href = "index.html");
     });
-}
-
-//Translation feature to go to english page
-translatePage.addEventListener('click', function () {
-    if (window.location.pathname === '/index.html') {
-        window.location.href = 'indexFR.html';
-    } else {
-        window.location.href = 'index.html';
-    }
-});
-
-//Code for collapsible projects
-//Code learned from W3
-var dropdowns = document.getElementsByClassName("dropdown");
-
-for (var i = 0; i < dropdowns.length; i++) {
+for (
+    var dropdowns = document.getElementsByClassName("dropdown"), i = 0;
+    i < dropdowns.length;
+    i++
+)
     dropdowns[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
+        var e = this.nextElementSibling;
+        "block" === e.style.display
+            ? (e.style.display = "none")
+            : (e.style.display = "block");
     });
-}
-
-//Typed in FR
-var typed = new Typed('.element', {
-    strings: ["Salut tout le monde! Je m'appelle Raj Patel et j'étudie la programmation informatique au Humber College. j'ai pris un un grand pas dans une nouvelle direction pour apprendre quelque chose de nouveau! Cette disciplinem'aidera éventuellement à faire un changement dans ma carrière. Je travaille actuellement sur quelques projets de développement Web en parallèle, en plus d'être un entrepreneur indépendant. Avant cela, j'ai été machiniste pendant cinq ans. J'attends avec impatience quoi et où ce nouveau voyage m'amènera, merci d'avoir visité ma page!!"],
+var typed = new Typed(".element", {
+    strings: [
+        "Salut tout le monde! Je m'appelle Raj Patel et j'étudie la programmation informatique au Humber College. j'ai pris un un grand pas dans une nouvelle direction pour apprendre quelque chose de nouveau! Cette disciplinem'aidera éventuellement à faire un changement dans ma carrière. Je travaille actuellement sur quelques projets de développement Web en parallèle, en plus d'être un entrepreneur indépendant. Avant cela, j'ai été machiniste pendant cinq ans. J'attends avec impatience quoi et où ce nouveau voyage m'amènera, merci d'avoir visité ma page!!",
+    ],
     typeSpeed: 10,
     startDelay: 500,
-    showCursor: true,
-    shuffle: true,
-
+    showCursor: !0,
+    shuffle: !0,
 });
-
-// Dark Mode
-const toggleThemeButton = document.getElementById('darkMode');
-toggleThemeButton.addEventListener('click', function () {
-    document.body.classList.toggle('dark-mode');
-});
-
-// Display Date-Time
+const toggleThemeButton = document.getElementById("darkMode");
 function displayTime() {
     function updateTime() {
-        let currentDate = new Date();
-        let formatTime = currentDate.toLocaleString();
-        document.getElementById('currentTime').innerText = formatTime;
+        let e = new Date().toLocaleString();
+        document.getElementById("currentTime").innerText = e;
     }
-    updateTime();
-    setInterval(updateTime, 1000);
+    updateTime(), setInterval(updateTime, 1e3);
 }
-displayTime();
+toggleThemeButton.addEventListener("click", function () {
+    document.body.classList.toggle("dark-mode");
+}),
+    displayTime();
